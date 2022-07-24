7e186f6d9124978836bf90b4f7d637de670db4c8
YesutoS <yesuto.shaw@kellogg.northwestern.edu>
2022-07-24 04:29:41 +0000

    Minor updates 1

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = app => {
   app.use(
     "/api",
     proxy({
+      /*target: process.env.REACT_APP_API_HOST || "https://8000-yesutos-plaidquickstart-twhy0wqwjtu.ws-us54.gitpod.io/",*/
       target: process.env.REACT_APP_API_HOST || "http://localhost:8000",
       changeOrigin: true
     })
